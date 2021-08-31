3c0e5cbeb21a61ad7a3c65bb6ea4984b50f6b8ed
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-08-31 20:59:34 +0300

    htmlise.bash: call python3 explicitly

--- htmlise.bash
+++ htmlise.bash
@@ -7,21 +7,21 @@ IFS=$'\n\t'
 
 find gemini -name '*.gmi' -exec sh -c 'gawk -v header=templates/header.html -v footer=templates/footer.html -f scripts/spaceh.awk {} > html/wormhole/$(basename -s .gmi {}).html' \;
 
-python scripts/g2ra.py static \
+python3 scripts/g2ra.py static \
     -u cadadr.space -a 'Göktuğ Kayaalp' -t atom -p \
     gemini/blag.gmi \
     > html/wormhole/blag.atom.xml
 
 sed -i "/<\\/style>/a \ \ \ \ <link rel='alternate' type='application/atom+xml' href='blag.atom.xml' title='Atom feed'/>" html/wormhole/blag.html 
 
-python scripts/g2ra.py static \
+python3 scripts/g2ra.py static \
     -u cadadr.space -a 'Göktuğ Kayaalp' -t atom -p \
     gemini/poesy.gmi \
     > html/wormhole/poesy.atom.xml
 
 sed -i "/<\\/style>/a \ \ \ \ <link rel='alternate' type='application/atom+xml' href='poesy.atom.xml' title='Atom feed'/>" html/wormhole/poesy.html 
 
-python scripts/g2ra.py static \
+python3 scripts/g2ra.py static \
     -u cadadr.space -a 'Göktuğ Kayaalp' -t atom -p \
     gemini/laurea-maestrale.gmi \
     > html/wormhole/laurea-maestrale.atom.xml
